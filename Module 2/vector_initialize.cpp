d6abd7c4dbe0b40a892265ae3dec8299d9ca37a1
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-29 16:08:21 +0600

    git initialize

--- Module 2/vector_initialize.cpp
+++ Module 2/vector_initialize.cpp
@@ -6,9 +6,10 @@ int main()
     // vector<int> v;  type 1
     // vector<int> v(5);    type 2
     // vector<int> v(5, 10);   type 3
-    vector<int> v1(5, 100);
-    vector<int> v(v1);
+    // vector<int> v1(5, 100);
+    // vector<int> v(v1);   type 4
 
+    vector<int> v = {10, 20, 30};
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
