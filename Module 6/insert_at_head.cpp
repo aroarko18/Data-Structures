5d2880bd3c346789ce583aaa5874490d56f1397d
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-08 15:31:32 +0600

    inserted at head in a linked list

---
+++ Module 6/insert_at_head.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        cout << endl
+             << endl
+             << "--------------Inserted at Head--------------" << endl
+             << endl;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    cout << endl
+         << endl
+         << "--------------Inserted at Tail--------------" << endl
+         << endl;
+}
+
+void print_linkedList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void insert_at_any_pos(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 0; i < pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    cout << endl
+         << endl
+         << "--------------Inserted at " << pos << " --------------" << endl
+         << endl;
+}
+
+void insert_at_head(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    newNode->next = head;
+    head = newNode;
+
+    cout << endl
+         << endl
+         << "--------------Inserted at Head --------------" << endl
+         << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << "Option 1: Insert at tail" << endl;
+        cout << "Option 2: Print Linked List" << endl;
+        cout << "Option 3: Insert at any position" << endl;
+        cout << "Option 4: Insert at Head" << endl;
+        cout << "Option 5: Terminate" << endl;
+
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            cout << "Enter your value: ";
+            int v;
+            cin >> v;
+            insert_at_tail(head, v);
+        }
+        else if (op == 2)
+        {
+            cout << "Your Linked List: ";
+            print_linkedList(head);
+        }
+        else if (op == 3)
+        {
+            int pos, val;
+            cout << "Enter position: ";
+            cin >> pos;
+            cout << "Enter value: ";
+            cin >> val;
+            if (pos == 0)
+            {
+                insert_at_head(head, val);
+            }
+            else
+            {
+                insert_at_any_pos(head, pos, val);
+            }
+        }
+        else if (op == 4)
+        {
+            int val;
+            cout << "Enter your value: ";
+            cin >> val;
+            insert_at_head(head, val);
+        }
+        else if (op == 5)
+        {
+            break;
+        }
+    }
+    return 0;
+}
