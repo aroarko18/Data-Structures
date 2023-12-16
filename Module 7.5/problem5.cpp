b81e4af3d139aa114279af4e3eff108338448393
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-17 01:04:47 +0800

    sorting in a descending order

---
+++ Module 7.5/problem5.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linkedList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+void sort_descending(Node *head)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val < j->val)
+            {
+                swap(i->val, j->val);
+            }
+        }
+    }
+    print_linkedList(head);
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int v;
+    while (true)
+    {
+        cin >> v;
+        if (v == -1)
+            break;
+        else
+        {
+            insert_at_tail(head, tail, v);
+        }
+    }
+    sort_descending(head);
+    return 0;
+}
