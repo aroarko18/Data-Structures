5f275e42209687e3b9f1251528794852a1ffc949
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-17 00:54:09 +0800

    max find in linked list

---
+++ Module 7.5/problem4.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void max_finder(Node *head)
+{
+    int max = INT_MIN;
+    for (Node *i = head; i != NULL; i = i->next)
+    {
+        if (i->val > max)
+        {
+            max = i->val;
+        }
+    }
+    cout << max << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int v;
+    while (true)
+    {
+        cin >> v;
+        if (v == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, tail, v);
+        }
+    }
+
+    max_finder(head);
+
+    return 0;
+}
