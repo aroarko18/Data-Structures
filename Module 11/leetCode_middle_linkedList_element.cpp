6546537d6aeb59fab256cc8c8a6b25181ac38a6d
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-22 23:07:33 +0800

    middle element of singly linked list

---
+++ Module 11/leetCode_middle_linkedList_element.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int size(ListNode *head)
+    {
+        int count = 0;
+        ListNode *tmp = head;
+        while (tmp != NULL)
+        {
+            count++;
+            tmp = tmp->next;
+        }
+        return count;
+    }
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *tmp = head;
+        int n = size(head);
+        for (int i = 1; i <= n / 2; i++)
+        {
+            tmp = tmp->next;
+        }
+        return tmp;
+    }
+};
