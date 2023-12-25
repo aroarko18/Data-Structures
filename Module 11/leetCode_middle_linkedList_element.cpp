dd878836e0f874b0f0c75e567b14c9c4828e697f
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-25 23:13:24 +0800

    slow and fast pointer

--- Module 11/leetCode_middle_linkedList_element.cpp
+++ Module 11/leetCode_middle_linkedList_element.cpp
@@ -32,4 +32,15 @@ public:
         }
         return tmp;
     }
+
+    /*
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    */
 };
