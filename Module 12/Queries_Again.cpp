906ac7120aff223d087f67e4f8ee3b48c006124a
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-27 00:30:21 +0800

    queries and insert at any pos

---
+++ Module 12/Queries_Again.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_head(Node *&head, Node *&tail, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+void insert_at_pos(Node *head, int pos, int v)
+{
+    Node *newNode = new Node(v);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->next->prev = newNode;
+    newNode->prev = tmp;
+}
+
+void insert_at_tail(Node *&head, Node *&tail, int v)
+{
+    Node *newNode = new Node(v);
+    if (tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = tail->next;
+}
+
+int size(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+void print_normal(Node *head)
+{
+    Node *tmp = head;
+    cout << "L -> ";
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    cout << "R -> ";
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int q;
+    cin >> q;
+    int x, v;
+    while (q--)
+    {
+        cin >> x >> v;
+        if (x == 0)
+        {
+            insert_at_head(head, tail, v);
+            print_normal(head);
+            print_reverse(tail);
+        }
+        else if (x == size(head))
+        {
+            insert_at_tail(head, tail, v);
+            print_normal(head);
+            print_reverse(tail);
+        }
+        else if (x > size(head))
+        {
+            cout << "Invalid" << endl;
+        }
+        else
+        {
+            insert_at_pos(head, x, v);
+            print_normal(head);
+            print_reverse(tail);
+        }
+    }
+
+    return 0;
+}
