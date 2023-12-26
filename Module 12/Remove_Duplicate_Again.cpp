8a29fa640a89a9144cfb915280f4914f888cfef0
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-12-26 17:42:07 +0800

    remove duplicate

---
+++ Module 12/Remove_Duplicate_Again.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> myList;
+    int v;
+    while (true)
+    {
+        cin >> v;
+        if (v == -1)
+            break;
+        myList.push_back(v);
+    }
+
+    myList.sort();
+    myList.unique();
+
+    for (int val : myList)
+    {
+        cout << val << " ";
+    }
+    return 0;
+}
